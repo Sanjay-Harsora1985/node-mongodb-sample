82720f98cdb8f8d45935bce63588089d2e195931
Sanjay-Harsora1985 <58841321+Sanjay-Harsora1985@users.noreply.github.com>
2021-04-07 21:19:37 +0530

    Update server.js

--- server.js
+++ server.js
@@ -28,7 +28,7 @@ mongoose.connect(dbConfig.url, {
 
 // define a simple route
 app.get('/', (req, res) => {
-    res.json({"message": "Welcome to Nodejs-with-MongoDB Demo session. Take notes quickly. Organize and keep track of all your notes."});
+    res.json({"message": "This is Demo session. Take notes quickly. Organize and keep track of all your notes."});
 });
 
 require('./app/routes/note.routes.js')(app);
